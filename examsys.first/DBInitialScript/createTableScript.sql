8019e4a9aefd0dd31b172adb08e5fe5b92d41d00
hongyuhang <hongyuhang@192.168.112.106>
2017-02-18 10:43:04 +0800

    增加了答题情况表中的试题类型字段

--- examsys.first/DBInitialScript/createTableScript.sql
+++ examsys.first/DBInitialScript/createTableScript.sql
@@ -54,6 +54,7 @@ CREATE TABLE `exam_question_tbl` (
   `testpaper_id` bigint(10) DEFAULT NULL,
   `question_content` varchar(200) NOT NULL,
   `seq` int(11) NOT NULL,
+  `question_type` int(2) DEFAULT NULL,
   `correct_flag` tinyint(2) NOT NULL COMMENT '0:不正确\n1:正确',
   PRIMARY KEY (`question_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='考试试题表';
